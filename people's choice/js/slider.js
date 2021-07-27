1e563961843b97936d6aa6f15215b547d907c43b
aditya687 <adityapanigrahy134@gmail.com>
2021-07-28 00:05:20 +0530

    created a new website

---
+++ people's choice/js/slider.js
@@ -0,0 +1,53 @@
+const glide1 = document.getElementById("glide1");
+const glide2 = document.getElementById("glide2");
+const glide3 = document.getElementById("glide3");
+
+if (glide1)
+  new Glide(glide1, {
+    type: "carousel",
+    startAt: 0,
+    autoplay: 3000,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: "linear",
+  }).mount();
+
+if (glide2)
+  new Glide(glide2, {
+    type: "carousel",
+    startAt: 0,
+    perView: 5,
+    hoverpause: false,
+    autoplay: 2000,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      1200: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+    },
+  }).mount();
+
+if (glide3) {
+  new Glide(glide3, {
+    type: "carousel",
+    startAt: 0,
+    perView: 3,
+    rewin: false,
+    autoplay: 3000,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      998: {
+        perView: 2,
+      },
+      768: {
+        perView: 1,
+      },
+    },
+  }).mount();
+}
