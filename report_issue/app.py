61e0a526af03f748d9465e5e0ebb84dd68e43fec
comnk <vhsiao36@gmail.com>
2021-08-03 18:25:30 -0700

    Create app.py

---
+++ report_issue/app.py
@@ -0,0 +1,33 @@
+from flask import *
+from email.message import EmailMessage
+import smtplib
+import os
+app = Flask(__name__)
+
+os.environ['API_USER'] = '' #insert gmail full email here (generate temp gmail account with password)
+os.environ['API_PASSWORD'] = '' # insert gmail email password here 
+
+API_USER = os.environ.get('API_USER')
+API_PASSWORD = os.getenv('API_PASSWORD')
+
+@app.route('/success', methods=['GET', 'POST'])
+def form():
+
+    message = request.form.get("message")
+    subject = request.form.get("subject")
+
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = 'jagannath9611@gmail.com'
+    msg['To'] = 'jagannath9611@gmail.com'
+    msg.set_content(message)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(API_USER, API_PASSWORD)
+
+        server.send_message(msg)
+
+    return render_template("email_success.html")
+
+if __name__ == '__main__':
+   app.run(host="127.0.0.1")
